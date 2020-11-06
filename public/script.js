040d5c030bc0bab7839a5be1097e1c281b1464a5
MaChanGit108 <50186087+MaChanGit108@users.noreply.github.com>
2020-11-06 12:34:42 -0500

    Updating script with new function and eventlistener

--- public/script.js
+++ public/script.js
@@ -1,17 +1,12 @@
-const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-const restaurants = []
-fetch(endpoint)
-    .then(resj => resj.json())
-    .then(data => restaurants.push(...data))
-
 function findMatches(wordToMatch, restaurants){
-    return restaurants.filter(place=> {
+    console.log(restaurants)
+    return restaurants.filter(place => {
         const regex = new RegExp(wordToMatch, 'gi');
         return place.restaurants.match(regex);
     });
 }
 
-function displayMatches(){
+function displayMatches(restaurants){
     const matchArray = findMatches(this.value, restaurants);
     const html = matchArray.map(place => {
         return `
@@ -25,11 +20,19 @@ function displayMatches(){
         </li>
     `;
     }).join('');
+    const suggestions = document.querySelector('.suggestions');
     suggestions.innerHTML = html;
 }
 
-const searchInput = document.querySelector('.text-input');
-const suggestions = document.querySelector('.suggestions');
+const searchInput = document.querySelector('.userinput');
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+async function loaddata() {
+    const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+    const restaurants = []
+    const data = await fetch(endpoint);
+    const resj = await data.json();
+    displayMatches(resj)
+    searchInput.addEventListener('keydown', displayMatches);
+}
+
+window.onload = loaddata;
